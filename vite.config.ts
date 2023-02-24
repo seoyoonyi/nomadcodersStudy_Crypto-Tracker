00d17a9560eb02666c3eb9b3cf919a12a37a7563
seoyoonyi <yiseoyoon@gmail.com>
2023-02-25 03:14:15 +0900

    fix:: 라우터 경로수정

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
 	plugins: [react()],
 	base: '/nomadcoderChallenge_Crypto-Tracker/',
-	define: {
-		'process.env': {},
-	},
+	// define: {
+	// 	'process.env': {},
+	// },
 });
